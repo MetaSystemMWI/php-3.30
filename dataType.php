7a4bbd35e5c582bc1f086c81cfd93ddfb8b79db1
MetaSystemMWI <128566079+MetaSystemMWI@users.noreply.github.com>
2023-11-16 16:39:14 +0500

    Add files via upload

---
+++ dataType.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // $x = "Hello world";
+    // $x = null;
+    // var_dump($x);
+
+    // echo strlen("Hello World");
+    // echo str_word_count("Hello Word");
+    // echo strrev("Hello World");
+    echo strpos("Hello world", "world");
+    ?>
+</body>
+</html>

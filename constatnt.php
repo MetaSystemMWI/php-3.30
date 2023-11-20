39a91a7ce132a42a0055cc1912dbbaace89f6f85
MetaSystemMWI <128566079+MetaSystemMWI@users.noreply.github.com>
2023-11-20 16:27:51 +0500

    Add files via upload

---
+++ constatnt.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // define('GREETING','welcome to meta system', true);
+    // echo greeting;
+
+
+    // const mycar = "BMW";
+    // echo mycar; 
+
+
+    // define ("cars",
+    // [
+    // "BMW",
+    // "Volvo",
+    // "Toyota"
+    // ]
+    // );
+    // echo cars[0];
+    // echo cars[1];
+    // echo cars[2];
+
+
+    define("GREETING", "WELCOME TO META SYSTEM");
+
+    // __CLASS__,
+    // __DIR__
+    // __FILE__
+    // __FUNCTION__
+    // __LINE__
+    // __METHOD__
+    // __NAMESPACE__
+    // __TRAIT__
+    // ::CLASS
+
+    // function myTest(){
+    //     echo GREETING;
+    // }
+    // myTest();
+
+
+    // class fruits{
+    //     public function myValue(){
+    //         return __CLASS__;
+           
+    //     }
+    // }
+    // $mango = new fruits();
+    // echo $mango->myValue();
+    // echo __DIR__;
+    // echo __FILE__;
+    // function myTest1(){
+    //     return __FUNCTION__;
+    // }
+    // echo myTest1();
+    // echo __LINE__;
+
+
+
+    // +,-,*,/,*,%,** : arithematic
+    // =, +=, -=, *=, /=, %=, assignement 
+    // ==, ===, !=, <> , !==, >, <,>=, <=, <=> camparision 
+    // ++,-- increment/ decrement
+    // and, or, xor, &&, ||, ! logical
+    // ., .= string 
+    // +, ==, === !=, <>, !== array 
+    // ?:, ?? conditional 
+    
+    ?>
+</body>
+</html>

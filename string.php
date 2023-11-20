f4da13262ec52205d81154ef1ffa574d274705e4
MetaSystemMWI <128566079+MetaSystemMWI@users.noreply.github.com>
2023-11-20 16:28:18 +0500

    Add files via upload

---
+++ string.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // echo str_replace("world","Dolly","Hello world");
+    // $x = 5898;
+    // var_dump(is_int($x));
+    // $y = 10;
+    // var_dump(is_float($y));
+    // $z = 1.9e411;
+    // var_dump($z);
+    // $u = acos(8);
+    // var_dump($u);
+
+
+    // echo "<br>";
+
+    // var_dump(is_numeric($x));
+
+    // // type casting 
+
+    // $k = 23465.786;
+    // $int_cast = (int)$k; 
+    // echo $int_cast;
+
+    // $i = "56556565";
+    // $int_cast = (int)$i;
+    // echo $int_cast;
+
+
+
+    // echo (pi());
+    // echo (max(0,150,30,20,-9,200));
+    // echo (min(0,150,30,20,-9,200));
+    // echo "<br>";
+    // echo (abs(-9));
+    // echo "<br>";
+    // echo (sqrt(64));
+    // echo "<br>";
+    // echo (round(9.51));
+    // echo "<br>";
+    // echo (rand());
+    // echo "<br>";
+    // echo (rand(10,100))
+
+
+    abs(),
+    acos(),
+    acosh()
+    asin()
+    asinh()
+    atan()
+    atan2()
+    atanh()
+    base_convert()
+    bindec()
+    ceil(),
+    cos(),
+    cosh(),
+    decbin()
+    dechex(),
+    decoct(),
+    deg2rad()
+    exp(),
+    expm1(),
+    floor(),
+    fmod(),
+    getrandmax(),
+    hexdec(),
+    hypot(),
+    intdiv(),
+    is_finite(),
+    is_infinite(),
+    is_nan(),
+    icg_value(),
+    
+    ?>
+
+    <!-- int_max, int_min, int_size, int, integer, long -->
+    <!-- float_max, float_min, Float_dig, float_epsilon, is_float, is_double; -->
+    <!-- infinity -->
+    <!-- nan -->
+</body>
+</html>

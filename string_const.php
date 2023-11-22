acc1087e1c92e2c374a098f447655db08d2431f0
MetaSystemMWI <128566079+MetaSystemMWI@users.noreply.github.com>
2023-11-22 16:01:14 +0500

    Add files via upload

---
+++ string_const.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+    // $t = "Hello";
+    // $x = "world";
+    // echo $t.$x;
+
+    // // $x = $x.$t;
+    // $x .= $t;
+    // echo $x;
+
+
+    $f = "red";
+    // switch($f){
+    //     case "red":
+    //         echo "Your favorit color is red";
+    //         break;
+    //     case "blue":
+    //         echo "Your favorit color is blue";
+    //         break;
+    //     case "green":
+    //         echo "Your favorit color is green";
+    //         break;  
+    //     default:
+    //         echo "wrong selection";
+    // }
+
+    // if($f=="red")
+    //     echo "red";
+    // else if($f=="blue")
+    //     echo "blue";
+    // else 
+    // echo "green";
+
+    $x =1 ;
+    while($x<5){
+        echo "the number is : $x <br>";
+        $x++;
+    }
+    ?>
+</body>
+</html>

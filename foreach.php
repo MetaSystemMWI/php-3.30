b89f69ba3c59222f2312c00ec7b06c92b7534a92
MetaSystemMWI <128566079+MetaSystemMWI@users.noreply.github.com>
2023-11-23 17:20:08 +0500

    Add files via upload

---
+++ foreach.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // $c = array("red","green","blue","yellow");
+
+        // foreach($c as $value){
+        //     echo "$value<br>";
+        // }
+
+
+        // $age = array("Ali"=>"35","Ahmed"=>"57","Asif"=>"56","Asim"=>"79");
+
+        // foreach($age as $x=>$value){
+        //     echo "$x = $value<br>";
+        // }
+
+        // for($x =0; $x<10; $x++){
+        //     if($x == 5){
+        //         // break;
+        //         continue;
+        //     }
+        //     echo "$x";
+        // }
+    ?>
+</body>
+</html>

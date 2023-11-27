575c0dab550fa3bad8dae612938b9ade95ac0e18
MetaSystemMWI <128566079+MetaSystemMWI@users.noreply.github.com>
2023-11-27 16:22:25 +0500

    Add files via upload

---
+++ array.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        // $car  = array("BMW","Toyata","Volvo","Hello","hi");
+        // // echo $car[0].",".$car[1].",".$car[2];
+        // $leg = count($car);
+        // for ($x =0; $x<$leg; $x++){
+        //     echo $car[$x];
+        //     echo "<br>";
+        // }
+
+
+        // $age = array("Ali"=> "16", "Ahmed"=>"20");
+        // // echo $age['Ali'];
+        // foreach($age as $x => $x_value){
+        //     echo $x."=". $x_value;
+        //     echo "<br>";
+        // }
+
+
+        $fruit = array(
+            array("Mango",200,1),
+            array("Orange",250,2),
+            array("banana",300,2),
+            array("Apple",350,1)
+        );
+
+        // echo $fruit[0][0], $fruit[0][1], $fruit[0][2];
+        // echo $fruit[1][0], $fruit[1][1], $fruit[1][2];
+        // echo $fruit[2][0], $fruit[2][1], $fruit[2][2];
+        // echo $fruit[3][0], $fruit[3][1], $fruit[3][2];
+
+
+        // for($row=0; $row<4; $row++){
+        //     echo "<p><b>Row Number $row </b></p>";
+        //     echo "<ul>";
+        //     for($col=0; $col<3; $col++){
+        //         echo "<li>".$fruit[$row][$col]."</li>";
+        //         // echo $fruit[$row][$col];
+        //         // echo "<br>";
+        //     }
+        //     echo "</ul>";
+        // }
+    ?>
+</body>
+</html>

08148be0713a2cea1d7adeee751f1b3f5e8e2852
MetaSystemMWI <128566079+MetaSystemMWI@users.noreply.github.com>
2023-11-28 16:42:46 +0500

    Add files via upload

---
+++ sort.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    Name: <input type="text" name="fname">
+    <input type="submit">
+</form> -->
+    <?php
+        // // $number = array(4,6,2,22,11);
+        // $number= array("Ali" => "34","Ahmed" => "56");
+        // // sort($number);
+        // // rsort($number);
+        // // asort($number);
+        // // ksort($number);
+        // // arsort($number);
+        // krsort($number);
+
+        // // $len = count($number);
+
+        // // for($x=0; $x<$len; $x++){
+        //     foreach($number as $x => $x_value){
+        //     echo $x,$x_value;
+        //     echo "<br>";
+        // }
+
+
+        // $GLOBALS, $_SERVER, $_REQUEST, $_POST, $_GET, $_FILES, $_ENV, $_COOKIE, $_SESSION
+
+        // $x = 75;
+        // $y = 25;
+        // function addition(){
+        //     $GLOBALS['z'] = $GLOBALS['x']+$GLOBALS['y'];
+        // }
+        // addition();
+        // echo $z; 
+        // echo $_SERVER['PHP_SELF'];
+        // ECHO "<BR>";
+        // echo $_SERVER['SERVER_NAME'];
+        // ECHO "<BR>";
+        // echo $_SERVER['HTTP_HOST'];
+        // ECHO "<BR>";
+        // echo $_SERVER['HTTP_REFERER'];
+        // ECHO "<BR>";
+        // echo $_SERVER['HTTP_USER_AGENT'];
+        // echo $_SERVER['SCRIPT_NAME'];
+        
+
+
+        // if($_SERVER["REQUEST_METHOD"]=="POST"){
+        //     $name = htmlspecialchars($_REQUEST['fname']);
+        //     if(empty($name)){
+        //         echo "Name is empty";
+        //     }else{
+        //         echo $name;
+        //     }
+        // }
+
+        
+      ?>
+      <!-- <a href="string_const.php?subject=PHP$web=gooogle.com">Get</a> -->
+</body>
+</html>

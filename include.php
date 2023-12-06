747b6f50b724e33ef83901584dfe2806e50f197f
MetaSystemMWI <128566079+MetaSystemMWI@users.noreply.github.com>
2023-12-06 16:24:55 +0500

    Add files via upload

---
+++ include.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <?php// include("date.php"); ?>
+    <?php //include 'function.php';  ?>
+    <?php //require 'function.php';  ?> 
+    <?php //require_once 'function.php';  ?>
+    <?php //readfile("new.txt"); ?>
+
+    <?php //$myfile = fopen("new.txt","w") or die("unable to open file!");
+        // echo fread($myfile,filesize("new.txt"));
+        // echo fgets($myfile);
+
+        // while(!feof($myfile)){
+        //     // echo fgets($myfile) . "<br>";
+        //     echo fgetc($myfile) . "<br>";
+        // }
+
+        // $text = "Ali Ahmed\n in miawnali";
+        // fwrite($myfile,$text);
+        // // echo fread($myfile,filesize("new.txt"));
+        // // echo fgets($myfile);
+        // fclose($myfile);
+    ?>
+</body>
+</html>
